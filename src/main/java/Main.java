a75938a9189e1c7a5241aa5c17f9724877d5f8d5
emike-akagbosu <emike.akagbosu20@imperial.ac.uk>
2022-11-22 16:52:27 +0000

    v1.1_devel

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        int count = 0;
+
         System.out.println("Hello, World!");
         for(int i = 1; i < 11; i++){
             System.out.println(i);
         }
+
+        for(int i = 30; i < 41; i++){
+            count += i;
+        }
+        System.out.println(count);
     }
 }
